41480d01bd40002d6e16e7fb8e20c95518c47093
Ashan Anuruddika <ashan.anuruddika@gmail.com>
2021-01-05 18:51:49 +0300

    IntraductionDetails was added.

---
+++ GedaratamaMalu/com.ashan.gedaratamamalu.view/IntraductionViewController.swift
@@ -0,0 +1,226 @@
+//
+//  IntraductionViewController.swift
+//  GedaratamaMalu
+//
+//  Created by Ashan Don on 12/28/20.
+//
+
+import UIKit
+
+struct IntraductionDetails {
+    
+    var image : UIImage!
+    
+    var quection : String!
+    
+    var intraduction : String!
+    
+    init(image : UIImage, quection : String, intraduction : String) {
+        
+        self.image = image
+        
+        self.quection = quection
+        
+        self.intraduction = intraduction
+        
+    }
+}
+
+class IntraductionViewController: UIViewController {
+
+    @IBOutlet weak var pageController: UIPageControl!
+    
+    @IBOutlet weak var swipingView: UICollectionView!
+    
+    private let intraductionList : [IntraductionDetails] = [
+        
+        IntraductionDetails(image: UIImage(named: "Intraduction1")!, quection: "Why Choose Us ?", intraduction: "Our main aim is to supply fresh seafood products to our valuable customers. All kinds of seafood sell on our website are from our daily catch and no added any preservatives. The kinds of seafood delivered to you are so fresh, delicious and at the lowest price of the market."),
+        
+        IntraductionDetails(image: UIImage(named: "Intraduction2")!, quection: "Payment Method ?", intraduction: "You do not require to pay us before you receive your product. Our friendly delivery staff delivery your order to your doorstep and payment can be made by cash or card upon delivery. So place your order today, and have delicious fresh seafood with your meal !!!"),
+        
+        IntraductionDetails(image: UIImage(named: "Intraduction3")!, quection: "Terms & Conditions ?", intraduction: "First of all please be honest to pay after receive your order. If you need to cancel any of your orders or If there is an issue with the order on arrival, please notify us as soon as possible.")
+        
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        swipingView.register(UINib(nibName: "SwipingViewCell", bundle: nil), forCellWithReuseIdentifier: "SWIPING_CELL")
+        
+        swipingView.dataSource = self
+        
+        swipingView.delegate = self
+        
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        setNavigationItem()
+        
+        setupPageController()
+        
+        
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+    }
+    
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        
+        let x = targetContentOffset.pointee.x
+        
+        let pageNumber = Int(x / swipingView.frame.width)
+        
+        pageController.currentPage = pageNumber
+        
+        if pageNumber == 0 {
+            
+            navigationItem.leftBarButtonItem?.title = "Skip"
+            
+        } else {
+            
+            navigationItem.leftBarButtonItem?.title = "Back"
+            
+        }
+    }
+    
+    private func setNavigationItem(){
+        
+        let leftNaviButton = UIBarButtonItem(title: "Skip", style: .plain, target: self, action: #selector(skipTapped))
+        
+        leftNaviButton.tintColor = UIColor(named: "Black_Color")
+        
+        navigationItem.leftBarButtonItem = leftNaviButton
+        
+        let rightNaviButton = UIBarButtonItem(title: "Next", style: .plain, target: self, action: #selector(nextTapped))
+        
+        rightNaviButton.tintColor = UIColor(named: "Black_Color")
+        
+        navigationItem.rightBarButtonItem = rightNaviButton
+        
+    }
+
+    @objc private func skipTapped(){
+        
+        if navigationItem.leftBarButtonItem?.title == "Skip" {
+            
+            presentSignInView()
+            
+        } else {
+            
+            handleBack()
+            
+        }
+    }
+    
+    @objc private func nextTapped(){
+        
+        navigationItem.leftBarButtonItem?.title = "Back"
+        
+        handleNext()
+    }
+    
+    private func setupPageController(){
+        
+        pageController.currentPage = 0
+        
+        pageController.numberOfPages = intraductionList.count
+        
+    }
+    
+    private func handleNext(){
+        
+        let nextIndex = min(pageController.currentPage + 1, intraductionList.count - 1)
+        
+        if pageController.currentPage == nextIndex {
+            
+            presentSignInView()
+            
+        } else {
+            
+            let indexPath = IndexPath(item: nextIndex, section: 0)
+            
+            pageController.currentPage = nextIndex
+            
+            let rect = swipingView.layoutAttributesForItem(at: indexPath)?.frame
+            
+            swipingView.scrollRectToVisible(rect!, animated: false)
+            
+        }
+        
+    }
+    
+    @objc private func handleBack(){
+        
+        let prevIndex = max(pageController.currentPage - 1, 0)
+        
+        let indexPath = IndexPath(item: prevIndex, section: 0)
+        
+        pageController.currentPage = prevIndex
+        
+        let rect = swipingView.layoutAttributesForItem(at: indexPath)?.frame
+        
+        swipingView.scrollRectToVisible(rect!, animated: false)
+        
+        if pageController.currentPage == 0 {
+            
+            navigationItem.leftBarButtonItem?.title = "Skip"
+            
+        }
+        
+    }
+    
+    private func presentSignInView(){
+        
+        let signInView = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "SIGNIN_SCREEN") as! SignInViewController
+        
+        signInView.modalTransitionStyle = .coverVertical
+        
+        signInView.modalPresentationStyle = .fullScreen
+        
+        self.navigationController?.present(signInView, animated: true, completion: nil)
+        
+    }
+}
+
+extension IntraductionViewController : UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return intraductionList.count
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let swippingCell = swipingView.dequeueReusableCell(withReuseIdentifier: "SWIPING_CELL", for: indexPath) as! SwipingViewCell
+        
+        swippingCell.contentView.frame = CGRect(x: 0, y: 0, width: swippingCell.frame.size.width, height: swippingCell.frame.size.height)
+        
+        let intraduction = intraductionList[indexPath.row]
+        
+        swippingCell.intraImageView.image = intraduction.image!
+        
+        swippingCell.intraTitleLabel.text = intraduction.quection
+        
+        swippingCell.intraDetailLabel.text = intraduction.intraduction
+        
+        return swippingCell
+        
+    }
+
+}
+
+extension IntraductionViewController : UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        
+        return 0
+        
+    }
+    
+}
