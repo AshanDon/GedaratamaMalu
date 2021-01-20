2f333a386a5c5db213c4505c79d080055b53bb8a
Ashan Anuruddika <ashan.anuruddika@gmail.com>
2021-01-20 23:16:57 +0300

    ProductViewCell was added.

---
+++ GedaratamaMalu/com.ashan.gedaratamamalu.controller/ProductViewCell.swift
@@ -0,0 +1,42 @@
+//
+//  ProductViewCell.swift
+//  GedaratamaMalu
+//
+//  Created by Ashan Don on 1/16/21.
+//
+
+import UIKit
+
+class ProductViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage :UIImageView!
+    @IBOutlet weak var catagoryNameLabel :UILabel!
+    @IBOutlet weak var productNameLabel :UILabel!
+    @IBOutlet weak var prouuctPrice :UILabel!
+    @IBOutlet weak var addCartButton :UIButton!
+    
+    var product : Product! {
+        didSet{
+            guard let productDetails = product else { return }
+            
+            self.productImage.image = UIImage(named: productDetails.productImage)
+            self.catagoryNameLabel.text = productDetails.catogaryName
+            self.productNameLabel.text = productDetails.productName
+            self.prouuctPrice.text = "රු\(productDetails.productPrice) per kg"
+        }
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        updateCellComponent()
+        
+    }
+
+    
+    private func updateCellComponent(){
+        
+        addCartButton.layer.cornerRadius = addCartButton.frame.height / 2
+        
+    }
+    
+}
