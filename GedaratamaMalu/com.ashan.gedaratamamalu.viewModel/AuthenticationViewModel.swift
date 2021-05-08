e213b1ef19adedb2f4a9bd76cace6d74c0b3c495
Ashan Anuruddika <ashan.anuruddika@gmail.com>
2021-05-08 09:49:20 +0300

    AuthenticationViewModel was updated.

--- GedaratamaMalu/com.ashan.gedaratamamalu.viewModel/AuthenticationViewModel.swift
+++ GedaratamaMalu/com.ashan.gedaratamamalu.viewModel/AuthenticationViewModel.swift
@@ -8,31 +8,52 @@
 import Foundation
 import Alamofire
 
-protocol AuthenticationDelegate {
+@objc protocol AuthenticationDelegate {
     
-    func getJwtToken(token : String)
-    
+    @objc optional func getJwtToken(token : String)
+    @objc optional func getSignInError(_ message : String)
 }
 
 class AuthenticationViewModel {
     //192.168.0.182
     private let baseURL : String = "http://192.168.0.182:8080/malukade/v1"
     
-    public var delegate : AuthenticationDelegate!
+    public var authDelegate : AuthenticationDelegate!
     
-    public func getDefaultJwtWebToken(){
+    fileprivate func getHttpHeaders() -> HTTPHeaders{
         let headers: HTTPHeaders = [
             .accept("application/json"),
             .contentType("application/json")
         ]
-        
+        return headers
+    }
+    
+    public func getDefaultJwtWebToken(){
 
         let auth = Authentication(userName: "AshanDon", password: "ashan123")
         
-        AF.request("\(baseURL)/authenticate",method: .post,parameters: auth,encoder: JSONParameterEncoder.default,headers: headers, interceptor: nil,requestModifier: nil).responseDecodable(of: JwtToken.self) { response in
+        AF.request("\(baseURL)/authenticate",method: .post,parameters: auth,encoder: JSONParameterEncoder.default,headers: getHttpHeaders(), interceptor: nil,requestModifier: nil).responseDecodable(of: JwtToken.self) { response in
             if let getData = response.value {
-                self.delegate.getJwtToken(token: getData.jwt)
+                self.authDelegate.getJwtToken!(token: getData.jwt)
             }
         }.resume()
+    }
+    
+    public func getUserProfile(_ userName : String, _ password : String){
+        
+        let auth = Authentication(userName: userName, password: password)
+        
+        AF.request("\(baseURL)/authenticate",method: .post,parameters: auth,encoder: JSONParameterEncoder.default,headers: getHttpHeaders(), interceptor: nil,requestModifier: nil).responseDecodable(of: JwtToken.self) { [weak self] response in
+            
+            guard let strongeSelf = self else { return }
+            
+            if let value = response.value {
+                strongeSelf.authDelegate.getJwtToken!(token: value.jwt)
+            } else {
+                strongeSelf.authDelegate.getSignInError!("Incorret user name or password")
+            }
+            
+        }.resume()
+        
     }
 }
