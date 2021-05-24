09a78ec06702ca6f5d7e277a03f919c9dd589f5c
Ashan Anuruddika <ashan.anuruddika@gmail.com>
2021-05-25 00:44:07 +0300

    Category file was added.

---
+++ GedaratamaMalu/com.ashan.gedaratamamalu.model/Category.swift
@@ -0,0 +1,26 @@
+//
+//  Category.swift
+//  GedaratamaMalu
+//
+//  Created by Ashan Don on 5/9/21.
+//
+
+import Foundation
+
+struct Category : Codable {
+    let id : Int?
+    let name : String?
+
+    enum CodingKeys: String, CodingKey {
+
+        case id = "id"
+        case name = "name"
+    }
+
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        id = try values.decodeIfPresent(Int.self, forKey: .id)
+        name = try values.decodeIfPresent(String.self, forKey: .name)
+    }
+
+}
