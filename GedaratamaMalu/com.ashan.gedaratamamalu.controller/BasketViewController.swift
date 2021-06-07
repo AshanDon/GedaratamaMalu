546e0df71fb623a1920730acfff601179411783c
Ashan Anuruddika <ashan.anuruddika@gmail.com>
2021-06-07 21:03:37 +0300

    BasketViewController was updated

--- GedaratamaMalu/com.ashan.gedaratamamalu.controller/BasketViewController.swift
+++ GedaratamaMalu/com.ashan.gedaratamamalu.controller/BasketViewController.swift
@@ -14,6 +14,9 @@ class BasketViewController: UIViewController {
     @IBOutlet weak var addItemButton : UIButton!
     @IBOutlet weak var checkoutButton : UIButton!
     @IBOutlet weak var bottomView : UIView!
+    @IBOutlet weak var specialRequestView : UIView!
+    @IBOutlet weak var noteTextView : UITextView!
+    @IBOutlet weak var scrollView : UIScrollView!
     
     @IBOutlet weak var cartTableHeightConstraint: NSLayoutConstraint!
     
@@ -22,12 +25,20 @@ class BasketViewController: UIViewController {
     public var cartList : [Product] = [Product]()
     
     fileprivate lazy var backButton : UIButton = {
-       let button = UIButton(frame: CGRect(x: 0, y: 0, width: 0, height: 44))
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 0, height: 44))
         button.setTitle(" Back", for: .normal)
         button.setImage(UIImage(named: "Arrow_Icon"), for: .normal)
         button.setTitleColor(UIColor(named: "Black_Color"), for: .normal)
         button.addTarget(self, action: #selector(backButtonDidPressed), for: .touchUpInside)
         return button
+    }()
+    
+    fileprivate lazy var touchView : UIView = {
+        let touchView = UIView()
+        touchView.frame = view.frame
+        touchView.isUserInteractionEnabled = true
+        touchView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(dismissKeyBoard)))
+        return touchView
     }()
     
     override func viewDidLoad() {
@@ -41,12 +52,38 @@ class BasketViewController: UIViewController {
         
         cartTableHeightConstraint.constant = (basketCellHeight * CGFloat(cartList.count))
         self.loadViewIfNeeded()
+        
+        scrollView.isScrollEnabled = true
 
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
         setupBasketScreen()
+        
+        registerKeyboardEvent()
+        
+    }
+    
+    
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        removeKeyboardEvent()
+        
+    }
+    
+    fileprivate func registerKeyboardEvent() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    fileprivate func removeKeyboardEvent() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
     }
 
     fileprivate func setupBasketScreen(){
@@ -92,7 +129,7 @@ class BasketViewController: UIViewController {
         let transition = CATransition()
         transition.duration = 0.5
         transition.type = .moveIn
-        transition.subtype = .fromRight
+        transition.subtype = .fromLeft
         transition.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
         self.view.window!.layer.add(transition, forKey: kCATransition)
         
@@ -114,6 +151,54 @@ class BasketViewController: UIViewController {
             
             window.rootViewController = tabbar
         }
+    }
+    
+    @objc fileprivate func keyboardWillShow(_ notification : Notification){
+        
+        view.addSubview(touchView)
+        
+        guard let keyboardFrame = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else { return }
+        
+        print(keyboardFrame.height)
+
+        let edgeInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: keyboardFrame.height , right: 0.0)
+        
+        scrollView.contentInset = edgeInsets
+        
+        scrollView.scrollIndicatorInsets = edgeInsets
+    }
+    
+    @objc fileprivate func keyboardWillHide(_ notification : Notification){
+        
+        touchView.removeFromSuperview()
+        
+        let edgeInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: 0.0 , right: 0.0)
+        
+        scrollView.contentInset = edgeInsets
+        
+        scrollView.scrollIndicatorInsets = edgeInsets
+    }
+    
+    @objc fileprivate func dismissKeyBoard(){
+        view.endEditing(true)
+    }
+    
+    @IBAction func checkoutButtonDidPress(_ sender: Any) {
+        let placerOrderView = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "PLACE_ORDER") as! PlaceOrderViewController
+        
+        placerOrderView.modalPresentationStyle = .fullScreen
+        
+        let transition = CATransition()
+        transition.duration = 0.5
+        transition.type = .push
+        transition.subtype = .fromRight
+        transition.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
+        
+        if let window = view.window {
+            window.layer.add(transition, forKey: kCATransition)
+        }
+        
+        present(placerOrderView, animated: false, completion: nil)
     }
     
 }
